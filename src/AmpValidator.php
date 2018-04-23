b332c4d5dd57fa8252ac73232ef18d5a0ea0df39
Nils Langner <nils.langner@leankoala.com>
2018-04-23 22:07:51 +0200

    added error message for npm install

--- src/AmpValidator.php
+++ src/AmpValidator.php
@@ -9,11 +9,17 @@ class AmpValidator
         $file = sys_get_temp_dir() . DIRECTORY_SEPARATOR . md5(microtime()) . '.html';
         file_put_contents($file, $htmlContent);
 
-        $command = 'node ' . __DIR__ . '/../validator/amp.js ' . $file;
-        exec($command, $output, $return);
+        $command = 'node ' . __DIR__ . '/../validator/amp.js ' . $file . ' 2>&1';
+        exec($command, $plainOutoput, $return);
         unlink($file);
 
-        $failures = json_decode($output[0], true);
+        $output = implode("\n", $plainOutoput);
+
+        if (strpos($output, "Cannot find module 'amphtml-validator'") !== false) {
+            throw new \RuntimeException('Unable to start anphtml-validator. Please run "npm install ind directory ' . realpath(__DIR__ . '/../validator/') . '"');
+        }
+
+        $failures = json_decode($output, true);
 
         if (!$failures) {
             return [];
